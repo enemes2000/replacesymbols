32b2aa7c8e47910b8d469b410e6320cd628f25c2
NJONG <DEV-NJONMI@orion.bdc.ca>
2020-09-27 09:51:10 -0400

    Revert "Add more features"
    This reverts commit c44b8b1ce1c75147f34726fc4588f6de7f158198.

--- src/test/java/com/github/enemes2000/kafka/connect/smt/ReplaceSymbolTest.java
+++ src/test/java/com/github/enemes2000/kafka/connect/smt/ReplaceSymbolTest.java
@@ -1,18 +1,15 @@
 package com.github.enemes2000.kafka.connect.smt;
 
-import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.data.SchemaBuilder;
 import org.apache.kafka.connect.data.Struct;
 import org.apache.kafka.connect.sink.SinkRecord;
 import org.junit.After;
 import org.junit.Test;
-import sun.awt.geom.AreaOp;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.github.enemes2000.kafka.connect.smt.ReplaceSymbol.CONFIG_DEF;
 import static org.junit.Assert.assertEquals;
 
 public class ReplaceSymbolTest {
@@ -25,15 +22,9 @@ public class ReplaceSymbolTest {
 
     @Test
     public void withSchema(){
-
-
         final Map<String, String> props = new HashMap<>();
         props.put("source.symbol.value", "_");
         props.put("destination.symbol.value", "$");
-        props.put("symbol.table.positions", "1,3");
-        props.put("symbol.field.positions", "1,2");
-        props.put("source.field.name", "my_field");
-        props.put("source.table.name", "my_topic_in_action_");
         recordReplaceSymbol.configure(props);
 
         final Schema schema = SchemaBuilder.struct()
@@ -45,7 +36,7 @@ public class ReplaceSymbolTest {
         value.put("admin_action_plan", "toto");
         value.put("actionplan", "toti");
 
-        final SinkRecord record = new SinkRecord(props.get("source.table.name"), 0, null, null, schema, value,0);
+        final SinkRecord record = new SinkRecord("test_mike_topic", 0, null, null, schema, value,0);
 
         final SinkRecord transformedRecord = recordReplaceSymbol.apply(record);
 
